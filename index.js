960d83778e11a85d3a459adcf7a6efceb9869635
Ren Ivey <haleyrivey@gmail.com>
2021-07-15 00:00:10 -0400

    Updated Readme. Updated lib folders.

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-// Including required packages.
 const { file } = require('@babel/types');
 const fs = require('fs');
 const inquirer = require ('inquirer');
 const jest = require('jest');
 
-// // Creating function to generate HTML file.
-const generateHTML = (answers) =>
-`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Team</title>
-</head>
-<body>
-    
-${answers.project_title} < - this is how to call in information 
+// Linking each js file.
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
 
-</body>
-</html>`;
-
-// Gathering all necessary user input. 
-
-
-    .then((answers) => {
-            let rmContent = generateHTML(answers);
-        
-            fs.writeFile('SAMPLE.HTML', rmContent, (err) =>
-            err ? console.log(err) : console.log("Successfully generated HTML file")
-            );
-        });
+// Begin Manager prompts. 
+const manager = new Manager();
